fa0da036ba5057d10d22c78ab1fd18aff8294919
konykon <tsagarikon@hotmail.com>
2020-10-07 18:22:18 +0200

    include components to pages

--- myblog-backend/src/server.js
+++ myblog-backend/src/server.js
@@ -1,38 +1,60 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-
-const articlesInfo = {
-    'learn-react': {
-        upvotes: 0,
-        comments: [],
-    },
-    'learn-node': {
-        upvotes: 0,
-        comments: [],
-    },
-    'my-thoughts-on-resumes': {
-        upvotes: 0,
-        comments: [],
-    },
-}
+import { MongoClient } from 'mongodb';
 
 const app = express();
 
+// app.get('/hello', (req, res) => res.send('Hello!'));
+
 app.use(bodyParser.json());
 
-app.post('/api/articles/:name/upvote', (req, res) => {
-    const articleName = req.params.name;
+app.get('/api/articles/:name', async (req, res) => {
+    try {
+        const articleName = req.params.name;
 
-    articlesInfo[articleName].upvotes += 1;
-    res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`);
+        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
+        const db = client.db('myblog');
+
+        const articleInfo = await db.collection('articles').findOne({ name: articleName })
+        res.status(200).json(articleInfo);
+
+        client.close();
+    } catch (error) {
+        res.status(500).json({ message: 'Error connecting to db', error });
+    }
+
+})
+
+app.post('/api/articles/:name/upvote', async (req, res) => {
+    try {
+        const articleName = req.params.name;
+
+        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
+        const db = client.db('myblog');
+
+        const articleInfo = await db.collection('articles').findOne({ name: articleName });
+        await db.collection('articles').updateOne({ name: articleName }, { 
+            '$set': {
+                upvotes: articleInfo.upvotes + 1,
+            },
+        });
+        const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName });
+
+        res.status(200).json(updatedArticleInfo);
+
+        client.close();
+    } catch (error) {
+        res.status(500).json({ message: 'Error connecting to db', error });
+    }
 });
 
-app.post('/api/articles/:name/add-comment', (req, res) => {
-    const {username, text} = req.body;
-    const articleName = req.params.name;
+// app.post('/api/articles/:name/add-comment', (req, res) => {
+//     const { username, text } = req.body;
+//     const articleName = req.params.name;
 
-    articlesInfo[articleName].comments.push({username, text});
-    res.status(200).send(articlesInfo[articleName]);
-});
+//     articlesInfo[articleName].comments.push({ username, text });
 
-app.listen(8000, () => console.log('listening on port 8000'));
+//     res.status(200).send(articlesInfo[articleName]);
+// });
+
+app.listen(8000, () => console.log('Listening on port 8000'));
--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import articleContent from './article-content';
 import ArticlesList from '../components/ArticlesList';
+import CommentsList from '../components/CommentsList';
+import UpvotesSection from '../components/UpvotesSection';
 import NotFoundPage from './NotFoundPage';
 
 const ArticlePage = ({ match }) => {
     const name = match.params.name;
     const article = articleContent.find(article => article.name === name);
+
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await fetch(`/api/articles/${name}`);
+            const body = await result.json();
+            setArticleInfo(body);
+        }
+        fetchData();
+    }, [name]);
 
     if (!article) return <NotFoundPage />
 
@@ -14,11 +27,21 @@ const ArticlePage = ({ match }) => {
     return (
         <>
             <h1>{article.title}</h1>
+            <UpvotesSection
+                articleName={name}
+                upvotes={articleInfo.upvotes}
+                setArticleInfo={setArticleInfo}
+            />
             {article.content.map((paragraph, key) => (
                 <p key={key}>{paragraph}</p>
             ))}
+            <CommentsList
+                comments={articleInfo.comments}
+            />
             <h3>Other Articles:</h3>
-            <ArticlesList articles={otherArticles} />
+            <ArticlesList
+                articles={otherArticles}
+            />
         </>
     );
 }
