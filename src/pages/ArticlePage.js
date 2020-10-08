9d0343ed2742eb1541793c97b19820c64c244e18
konykon <tsagarikon@hotmail.com>
2020-10-08 17:08:45 +0200

    remove react demo

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -4,6 +4,7 @@ import ArticlesList from '../components/ArticlesList';
 import CommentsList from '../components/CommentsList';
 import UpvotesSection from '../components/UpvotesSection';
 import NotFoundPage from './NotFoundPage';
+import AddCommentForm from '../components/AddCommentForm';
 
 const ArticlePage = ({ match }) => {
     const name = match.params.name;
@@ -38,6 +39,7 @@ const ArticlePage = ({ match }) => {
             <CommentsList
                 comments={articleInfo.comments}
             />
+            <AddCommentForm articleName={name} setArticleInfo={setArticleInfo}/>
             <h3>Other Articles:</h3>
             <ArticlesList
                 articles={otherArticles}
