d700f74cae182d42343dff19acae6d927b6e0a6c
konykon <tsagarikon@hotmail.com>
2020-10-16 15:03:18 +0200

    minimum change

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -10,8 +10,7 @@ const AboutPage = () => (
         commodo consequat. Duis aute irure dolor in reprehenderit
         in voluptate velit esse cillum dolore eu fugiat nulla
         pariatur. Excepteur sint occaecat cupidatat non proident,
-        sunt in culpa qui officia deserunt mollit anim id est
-        laborum.</p>
+        sunt in culpa.</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
         sed do eiusmod tempor incididunt ut labore et dolore
         magna aliqua. Ut enim ad minim veniam, quis nostrud
